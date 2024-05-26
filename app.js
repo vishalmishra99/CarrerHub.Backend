affb9cf516a826778b75e9ac613da2afd370cca4
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-26 09:11:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config({path: "./config/config.env"});
 
 app.use(cors({
-    origin: ['https://career-hub-front-end.vercel.app','/api/v1/user/getuser']
+    origin: 'https://career-hub-front-end.vercel.app',
     methods: ["GET","POST","DELETE","PUT"],
     credentials: true,
   })
